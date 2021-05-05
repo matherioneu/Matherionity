cdef816430af964078442b957d47d78bdf298067
Mia <vottus@vott.us>
2021-05-05 03:34:48 +0200

    feat(rs-dev): compile the server on start

--- rs-dev/src/main.rs
+++ rs-dev/src/main.rs
@@ -1,22 +1,43 @@
 const JAVA_PATH: &str = "/usr/bin/java";
-const TARGET_JAR_PATH: &str = "../Tuinity-Server/target/matherionity-1.16.5.jar";
+const MVN_PATH: &str = "/usr/bin/mvn";
+const PARENT_PATH: &str = "../";
+const SPIGOT_SERVER_PATH: &str = "../Tuinity-Server";
 const DEV_SERVER_PATH: &str = "../dev-server";
-
-extern crate execute;
 
 use std::process::Command;
 
 use execute::Execute;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::fs::{create_dir, metadata, File};
-use std::env::{set_current_dir};
+use std::env::{set_current_dir, current_dir};
 use std::io::Write;
+use project_root::{get_project_root};
 
+/// This simple Rust app uses Maven CLI to compile
+/// the API & Server, then starts the server automatically.
+/// It is recommended to get a Rust integration for your IDE,
+/// so it is easy for you to execute this app.
+
+/// Let's go
 fn main() {
     // Create the dev server path if it doesn't exist
     if !metadata(DEV_SERVER_PATH).is_ok() {
+        println!("Creating {}", DEV_SERVER_PATH);
         create_dir(DEV_SERVER_PATH);
     }
+
+    let root_dir: PathBuf = get_project_root().unwrap();
+
+    // Compile the parent
+    println!("Running `package` against the parent ({})", PARENT_PATH);
+    set_current_dir(PARENT_PATH);
+    run_command(
+        Command::new(MVN_PATH)
+            .arg("package")
+    );
+
+    // Go back
+    set_current_dir(root_dir);
 
     // Set the current dir to the path of the dev server
     set_current_dir(DEV_SERVER_PATH);
@@ -26,11 +47,14 @@ fn main() {
     eula.unwrap().write_all(b"eula=true");
 
     // Start the server
-    let mut command = Command::new(JAVA_PATH);
+    run_command(
+        Command::new(JAVA_PATH)
+            .arg("-jar")
+            .arg(format!("{}{}", SPIGOT_SERVER_PATH, "/target/matherionity-1.16.5.jar"))
+    );
+}
 
-    command.arg("-jar");
-    command.arg(TARGET_JAR_PATH);
-
+fn run_command(command: &mut Command) {
     let output = command.execute_output().unwrap();
 
     // End the process
