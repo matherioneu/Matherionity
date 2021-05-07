d8b6efb717821369bbd7f11ec8d5cc6e7f2378b6
Mia <vottus@vott.us>
2021-05-07 02:28:58 +0200

    refactor: fix serverstate changes (for real), add pub/sub for state changes

---
+++ test-plugin/src/main/java/eu/matherion/plugin/Scratch.java
@@ -0,0 +1,18 @@
+package eu.matherion.plugin;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.matherion.api.ServerState;
+import eu.matherion.api.StateType;
+
+import java.io.IOException;
+
+class Scratch {
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(mapper.canSerialize(ServerState.class));
+        String json = mapper.writeValueAsString(new ServerState(2, "TheBridge", null, StateType.WAITING, false, 0, 10));
+        System.out.println(json);
+        ServerState rejsoned = mapper.readValue(json, ServerState.class);
+        System.out.println(rejsoned);
+    }
+}
